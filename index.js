18d021d7ddb987a7a5019fd27b37cae62e66eb9e
Emmanuel Pire <piremmanuel@gmail.com>
2020-04-25 23:28:00 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const axios = require('axios')
+const cron = require('node-cron');
+const { getLatestTweets, postReply } = require('./twitterLib');
+
+function pause(ms){
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+async function getText(){
+  const resp = await axios.get('https://automotron-v2.lipsumar.io/api/generators/0Shh5ZhGe/run')
+  return resp.data.text
+}
+
+cron.schedule('* 12,17 * * *', async () => {
+
+  await pause(60*1000 + Math.random()*20*60*1000)
+  try{
+    const [lastTweet] = await getLatestTweets('robertessayer', 1);
+    const selfLatestTweets = await getLatestTweets('bonpublic', 5);
+    const tweetReplyFromSelf = selfLatestTweets.find(tweet => tweet.in_reply_to_status_id_str === lastTweet.id_str)
+    if(tweetReplyFromSelf){
+      console.log('already replied...')
+    }else{
+      console.log('not yet replied!')
+      const text = getText()
+      postReply(text, lastTweet)
+      console.log('Tweeted: ' + text)
+    }
+    
+  }catch(err){
+    console.log('Error!', err)
+  }
+
+});
